2890cd911541bb494d73b48e68da794e4ec0495a
csxuejin <602296435@qq.com>
2019-10-14 17:18:24 +0800

    add code

--- demo/go.mod
+++ demo/go.mod
@@ -1,3 +1,6 @@
 module demo
 
-require git.jd.com/jdcloud-epn/epnc-common/iptool v0.0.0-20190827094254-6cc01022a37f // indirect
+require (
+	git.jd.com/jdcloud-epn/epnc-common/iptool v0.0.0-20190827094254-6cc01022a37f // indirect
+	github.com/csxuejin/iptool v0.0.0-20191014083946-d0fafd0c29c1 // indirect
+)
--- demo/main.go
+++ demo/main.go
@@ -11,7 +11,17 @@ var (
 )
 
 func IndexPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Your public ip is: %v\n", iptool.GetIPAddress(&r.Header))
+	ip := client.GetIPAddress(&r.Header)
+	fmt.Fprintf(w, "Your public ip is: %v\n", ip)
+
+	if ip != ""{
+		res, err := client.GetIPGeoInfo(ip)
+		if err != nil{
+			return
+		}
+
+		fmt.Fprintf(w, "Geographical location information is: %#v\n", res.Data)
+	}
 }
 
 func main() {
